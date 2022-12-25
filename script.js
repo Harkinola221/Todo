331f2f68c6f224bb210d8f1bc702cf40e49a5cf8
Michael Olabisi <michaelolabisi@Michael-Olabisi.local>
2022-12-25 14:13:29 +0100

    new file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector("#push").addEventListener('click', function(){
     alert("Please Enter a Task");
   } else {
     document.querySelector("#tasks").innerHTML += `
-      <div class="task id="draggable">
+      <div class="task" draggable="true">
         <span id="taskname">
           ${document.querySelector("#newtask input").value}
         </span>
@@ -13,26 +13,36 @@ document.querySelector("#push").addEventListener('click', function(){
       </div>
     `;
 
+
+    // Delete task not needed
     let current_tasks = document.querySelectorAll(".delete");
     for (let i = 0; i < current_tasks.length; i++) {
       current_tasks[i].addEventListener('click', function(){
         this.parentNode.remove();
       })
 
-      let dragged = document.querySelectorAll('.newtask')
-      for(let i = 0; i < dragged.length; i++){
-        dragged[i].addEventListener('dragStart', function(){
-          const dragged = this;
-        })
-      }
-    }
-    var tasks = document.querySelectorAll(".task");
-    for (let i = 0; i < tasks.length; i++) {
-      tasks[i].addEventListener('click', function(){
-        this.classList.toggle("completed");
-      })
-    }
+    };
 
+    // Change Background Color when clicked
+    let newTask = document.querySelectorAll(".task")
+    for (let a = 0; a < newTask.length; a++){
+      newTask[a].addEventListener('click', function(){
+        newTask[a].style.backgroundColor = '#8052ec'
+        newTask[a].style.borderRadius = '5px'
+      })
+    };
+
+    
+    
+    // // Stroke line when task is completed
+    // var tasks = document.querySelectorAll(".task");
+    // for (let i = 0; i < tasks.length; i++) {
+    //   tasks[i].addEventListener('click', function(){
+    //     this.classList.toggle("completed");
+    //   })
+    // }
+
+    // Makes input field empty after task as been called
     document.querySelector("#newtask input").value = "";
   }
 });
